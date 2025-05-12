bcc2fcb70f2d5062ff9ec36167726e5542acc2a7
Jia Shang <ohjiashang@gmail.com>
2025-05-12 17:41:09 +0800

    cleaned up repo

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from tabs import MR_live_tab, MR_top_diffs_tab
 from utils.constants import DIFFS_TO_TRACK_MAP
 
 st.set_page_config(layout="wide")
-st.title("Mean Reversion")
+st.title("Mean Reversion Model")
 
 tab1, tab2 = st.tabs(["Top Diffs", "Backtest by Diff"])
 
