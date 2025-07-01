072663147ca87b20ef591db2c32f10d6cf97fe61
Jia Shang <ohjiashang@gmail.com>
2025-07-01 15:14:50 +0800

    change documentation rendering to html

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import streamlit as st
 from tabs import MR_live_tab, MR_top_diffs_tab
 from utils.constants import DIFFS_TO_TRACK_MAP, DIFFS_TO_TRACK_MAP_OR
+from utils.documentation import display_doc
 import warnings
 # warnings.filterwarnings("ignore", message=".*missing ScriptRunContext.*")
 warnings.filterwarnings("ignore")
-import urllib.parse
-
 
 st.set_page_config(layout="wide", page_title="MeanReversion")
 st.title("Mean Reversion")
@@ -23,12 +22,5 @@ with tab3:
 
 with tab4:
     folder = "Test"
-    filename = f"Mean Reversion Model_Documentation_20250630.pdf"
-    encoded_filename = urllib.parse.quote(filename)
-    url = f"https://firebasestorage.googleapis.com/v0/b/hotei-streamlit.firebasestorage.app/o/{folder}%2F{encoded_filename}?alt=media"
-
-    st.markdown(
-        f'<iframe src="{url}" width="850" height="1000" type="application/pdf"></iframe>',
-        unsafe_allow_html=True
-    )
-
+    filename = f"Mean Reversion Model_Documentation_20250630.htm"
+    display_doc(folder, filename)
