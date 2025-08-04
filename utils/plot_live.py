933c9a91b4efe558e996d1f08db1a2e8b219088e
Jia Shang <ohjiashang@gmail.com>
2025-08-04 19:11:30 +0800

    alter chart

--- utils/plot_live.py
+++ utils/plot_live.py
@@ -91,10 +91,21 @@ def plot_live_contract_roll_plotly(df, selected_diff, selected_contract, selecte
     filtered_df = df[(df['Date'] > start_date) & (df['Date'] <= end_date)]
 
     ##### PLOTTING with Plotly #####
+    # Set colors based on theme
+    color = "black"
+    
     fig = go.Figure()
 
     # Plot Price line
-    fig.add_trace(go.Scatter(x=filtered_df['Date'], y=filtered_df[price_col], mode='lines', name='Price', line=dict(color='black', width=2.2)))
+    # fig.add_trace(go.Scatter(x=filtered_df['Date'], y=filtered_df[price_col], mode='lines', name='Price', line=dict(color='black', width=2.2)))
+    # Add trace with dynamic color
+    fig.add_trace(go.Scatter(
+        x=filtered_df['Date'],
+        y=filtered_df[price_col],
+        mode='lines',
+        name='Price',
+        line=dict(color=color, width=2.2)
+    ))
 
     # Plot rolling median, upper, and lower bounds
     if not filtered_df['rolling_median'].isnull().all():
@@ -128,9 +139,16 @@ def plot_live_contract_roll_plotly(df, selected_diff, selected_contract, selecte
         f"No. SD: {num_sd_formatted}<br>"
     )
 
+    # Add transparent annotation
     fig.add_annotation(
-        x=1.15, y=1, xref="paper", yref="paper", text=annotation_text, showarrow=False,
-        font=dict(size=15, color="black"), align="left", bgcolor="white", borderpad=4, bordercolor="white", borderwidth=1
+        x=1.15, y=1,
+        xref="paper", yref="paper",
+        text=annotation_text,
+        showarrow=False,
+        font=dict(size=15),
+        align="left",
+        bgcolor="rgba(0,0,0,0)",           # Fully transparent background
+        borderpad=4,
     )
 
     # Add individual point annotations
