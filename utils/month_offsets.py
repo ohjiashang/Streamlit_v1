a889a0391120e3ceaaf87bda4c15dc79503a516c
Jia Shang <ohjiashang@gmail.com>
2025-09-01 14:30:21 +0800

    roll mr

--- utils/month_offsets.py
+++ utils/month_offsets.py
@@ -144,8 +144,7 @@ def get_price_series(diff_scenario, months_scenario, months_m1_lst, years):
     # Step 1: Ordered task list
     task_list = [(year, month_m1) 
                 for year in reversed(years) 
-                for month_m1 in reversed(months_m1_lst)
-                if not (year == 25 and month_m1 in ['Dec'])]
+                for month_m1 in reversed(months_m1_lst)]
 
     def fetch_contract(year, month_m1):
         df_contract = process_contract(year, month_m1)
